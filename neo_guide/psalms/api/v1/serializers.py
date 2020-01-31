47145b74112d44dbe103acfda70c6d64a431b6c6
Szymon Miks <miks.szymon@gmail.com>
2020-01-31 12:20:53 +0100

    [trello-4]: Added improvments to serializers and views
    - changed queryset
    - add default_image to the serializer

--- neo_guide/psalms/api/v1/serializers.py
+++ neo_guide/psalms/api/v1/serializers.py
@@ -1,26 +1,35 @@
-from rest_framework.serializers import ModelSerializer
+from django.conf import settings
+from rest_framework import serializers
 
 from neo_guide.psalms.models import Psalm
 from neo_guide.psalms.models import PsalmAudio
 from neo_guide.psalms.models import PsalmImage
 
 
-class PsalmImageSerializer(ModelSerializer):
+class PsalmImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PsalmImage
-        fields = ('image',)
+        fields = ('image', 'active', 'default')
 
 
-class PsalmAudioSerializer(ModelSerializer):
+class PsalmAudioSerializer(serializers.ModelSerializer):
     class Meta:
         model = PsalmAudio
-        fields = ('audio',)
+        fields = ('audio', 'active')
 
 
-class PsalmSerializer(ModelSerializer):
+class PsalmSerializer(serializers.ModelSerializer):
     recordings = PsalmAudioSerializer(source='psalm_audio', many=True, read_only=True)
     images = PsalmImageSerializer(source='psalm_image', many=True, read_only=True)
+    default_image = serializers.SerializerMethodField()
+
+    def get_default_image(self, psalm):
+        if psalm.default_image:
+            default_image_url = f'{settings.MEDIA_URL}{psalm.default_image}'
+            return self.context['request'].build_absolute_uri(default_image_url)
+
+        return None
 
     class Meta:
         model = Psalm
-        fields = ['name', 'page_number', 'card_color', 'recordings', 'images']
+        fields = ('name', 'page_number', 'card_color', 'recordings', 'images', 'default_image')
--- neo_guide/psalms/api/v1/views.py
+++ neo_guide/psalms/api/v1/views.py
@@ -1,9 +1,13 @@
-from django.db.models import Q
+from django.db.models import OuterRef
+from django.db.models import Prefetch
+from django.db.models import Subquery
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from neo_guide.psalms.api.v1.serializers import PsalmSerializer
 from neo_guide.psalms.models import Psalm
+from neo_guide.psalms.models import PsalmAudio
+from neo_guide.psalms.models import PsalmImage
 
 
 class PsalmViewSet(viewsets.ReadOnlyModelViewSet):
@@ -13,12 +17,15 @@ class PsalmViewSet(viewsets.ReadOnlyModelViewSet):
     ordering_fields = ('name', 'card_number')
     serializer_class = PsalmSerializer
     queryset = (
-        Psalm.objects.filter(
-            (
-                Q(active=True)
-                & (Q(psalm_audio__active=True) | Q(psalm_image__default=True) | Q(psalm_audio__active=True))
+        Psalm.objects.prefetch_related(
+            Prefetch('psalm_audio', queryset=PsalmAudio.objects.filter(active=True)),
+            Prefetch('psalm_image', queryset=PsalmImage.objects.filter(active=True)),
+        )
+        .annotate(
+            default_image=Subquery(
+                PsalmImage.objects.filter(active=True, default=True, psalm=OuterRef('id')).values('image')
             )
         )
+        .filter(active=True)
         .order_by('page_number')
-        .distinct()
     )
